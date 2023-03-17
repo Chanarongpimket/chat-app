2f0ed4f1a9c76e90bc86565c6a1977e874fc9e47
ChanarongPimket <chanarongpimket@gmail.com>
2023-03-18 06:20:21 +0700

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.scss'
+import Login from './pages/Login'
+import Register from './pages/Register'
+
+function App() {
+  return (
+    <div>
+      <Register/>
+    </div>
+  )
+}
+
+export default App
