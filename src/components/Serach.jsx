994389140be38b58f9fd15cc06e1fd355188a2b3
ChanarongPimket <chanarongpimket@gmail.com>
2023-03-25 01:58:18 +0700

    update components

---
+++ src/components/Serach.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Serach = () => {
+  return (
+    <div className="search">
+      <div className="serchForm">
+        <input type="text" />
+      <div className="userChat">
+        <img src="" alt="" />
+        <div className="userChatInfo">
+          <span>Jane</span>
+        </div>
+      </div>
+      </div>
+    </div>
+  )
+}
+
+export default Serach
